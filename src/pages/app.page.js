de92a60c98d756885d5c3bb2b69668ed69d5b076
Lida <l.karpova@freshauto2.ru>
2024-11-07 13:56:35 +0300

    доработки теста

--- src/pages/app.page.js
+++ src/pages/app.page.js
@@ -1,9 +1,10 @@
-import { MainPage, SearchPage } from "./index";
+import { MainPage, SearchPage, ResultPage } from "./index";
 
 export class App{
     constructor(page){
         this.page = page;
         this.mainPage = new MainPage(page);
         this.searchPage = new SearchPage(page);
+        this.resultPage = new ResultPage(page);
     }
 }
